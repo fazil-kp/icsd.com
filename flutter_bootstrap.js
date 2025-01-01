8067a629b8c3d352687b010062f6ac380920f378
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-01-01 23:39:17 +0530

    event card created

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "763072628"
+    serviceWorkerVersion: "2040182978"
   }
 });
