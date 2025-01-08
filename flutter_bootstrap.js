7b5a627d7a2beabae40d8576b9e150aee64b4e57
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-01-08 08:47:59 +0530

    new version hosting

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1419836520"
+    serviceWorkerVersion: "3115614006"
   }
 });
